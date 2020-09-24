e277946008a711754311708274fe22a8dfbd8f3c
Suvam Mohapatra <suvammohapatra8888@gmail.com>
2020-09-24 08:30:08 +0530

    Full Functional Prototype Tested Ok. Working on extra Functions and UI enhancements.

---
+++ src/main/java/com/mrperfect/helper/Helper.java
@@ -0,0 +1,22 @@
+
+package com.mrperfect.helper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+public class Helper {
+    
+    // For Getting Date User Friendly Style
+    public static String getOnlyDate(Date date){
+    
+    
+      SimpleDateFormat formatter= new SimpleDateFormat("dd MMMM yyyy");
+        String format = formatter.format(date);
+    return format;
+    }
+    }
